1d81d37c277f5a6c23c1172a3647b55b55f37e1f
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-05-30 10:32:34 +0200

    erwyt
    wy45

---
+++ Iterator/Iterator/ClientCode.cpp
@@ -0,0 +1,1 @@
+#include "ClientCode.h"
---
+++ Iterator/Iterator/Iterator.cpp
@@ -0,0 +1,16 @@
+/* Wzorzec projektowy : Iterator
+Za³o¿enie:
+Przemieszczanie siê po jakichkolwiek kolekcjach jak po liœcie
+*/
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Iterator.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
